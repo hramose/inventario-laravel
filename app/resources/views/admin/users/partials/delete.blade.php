770ee028ddc776f47a4ff1f02d16222a1b361610
sage393 <sage393@gmail.com>
2015-11-23 23:32:50 -0600

    se eliminaron ficheros fuera de lugar

--- app/resources/views/admin/users/partials/delete.blade.php
+++
@@ -1,5 +0,0 @@
-{!! Form::open(['route' => ['usuarios.destroy', $user], 'method' => 'DELETE']) !!}
-<button type="submit" class="btn btn-default  btn-sm  btn-sp" onclick="return confirm('¿Seguro que desea eliminar?')" >
-	<i class="glyphicon glyphicon-remove"></i>&nbsp;&nbsp;&nbsp;Eliminar
-</button>
-{!! Form::close() !!}
--- app/resources/views/admin/users/partials/filterForm.blade.php
+++
@@ -1,6 +0,0 @@
-{!! Form::open(['route' => ['usuarios.show', 'busqueda'], 'method' => 'GET', 'class' => 'navbar-form navbar-left pull-right', 'role' => 'search' ]) !!}
-<div class="form-group">
-	{!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Buscar usuario']) !!}
-</div>
-<button type="submit" class="btn btn-default">Buscar</button>
-{!! Form::close() !!}
