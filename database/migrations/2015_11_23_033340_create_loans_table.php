21595807c749bd40ffcbe12ac4a47a193241a2b2
root <root@gosamt.sagelap>
2015-11-25 14:31:36 -0600

    se elimina tokken y se agregan campos date

--- database/migrations/2015_11_23_033340_create_loans_table.php
+++ database/migrations/2015_11_23_033340_create_loans_table.php
@@ -18,11 +18,12 @@ class CreateLoansTable extends Migration {
 			$table->increments('id');
 			$table->integer('cantidad');
 			$table->string('descripcion');
+			$table->date('inicio_prestamo');
+			$table->date('fin_prestamo');
 //------------------------------------------------------------------------------
 			$table->integer('products_id')->unsigned();
 			$table->foreign('products_id')->references('id')->on('products');
 //------------------------------------------------------------------------------
-			$table->rememberToken();
 			$table->softDeletes(); // Campo para borrado logico
 			$table->timestamps();
 //------------------------------------------------------------------------------
