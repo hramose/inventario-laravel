f1952037e020dd11068d94f40649f388307e2bf0
root <root@gosamt.sagelap>
2015-11-26 10:54:43 -0600

    se creo middleware para usuarios admin

---
+++ app/Http/Middleware/SoloAdmin.php
@@ -0,0 +1,44 @@
+<?php namespace Inventario\Http\Middleware;
+
+use Closure;
+use Illuminate\Contracts\Auth\Guard;
+
+class SoloAdmin {
+
+	/**
+	* The Guard implementation.
+	*
+	* @var Guard
+	*/
+
+	protected $auth;
+
+	/**
+	* Create a new filter instance.
+	*
+	* @param  Guard  $auth
+	* @return void
+	*/
+	public function __construct(Guard $auth)
+	{
+		$this->auth = $auth;
+	}
+
+	/*
+	* Handle an incoming request.
+	*
+	* @param  \Illuminate\Http\Request  $request
+	* @param  \Closure  $next
+	* @return mixed
+	*/
+	public function handle($request, Closure $next)
+	{
+		if ($this->auth->user()->rol != 'administrador')
+		{
+			\Session::flash('message', 'Intento entrar a una opción disponible solo para administradores del sistema' );
+			return redirect()->to('/');
+		}
+		return $next($request);
+	}
+
+}
