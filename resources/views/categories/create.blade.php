a6d75148bfe98abf5fbafc37d57de5f331973d7a
sage393 <sage393@gmail.com>
2015-11-27 09:47:59 -0600

    se cambio texto

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -8,7 +8,7 @@ Nuevo Usuario
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
-				<div class="panel-heading">Nuevo Usuario</div>
+				<div class="panel-heading">Nueva Categoria</div>
 
 				@include('partials.validateMessage')
 
