77d52cba60801e9728f65e4a543fc976a6eb61a2
sage393 <sage393@gmail.com>
2015-11-26 18:50:27 -0600

    se crearon Request para las clases

---
+++ app/Http/Requests/EditLoanRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Inventario\Http\Requests;
+
+use Inventario\Http\Requests\Request;
+
+class EditLoanRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+}
