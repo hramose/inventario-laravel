ffa8eb36b52faea3183a1e0fd184fc0f1a925d6c
sage393 <sage393@gmail.com>
2015-11-27 09:46:14 -0600

    se cambiaron nombres de variables

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -7,20 +7,20 @@ Usuarios
 	<div class="row">
 		<div class="col-md-12">
 			<div class="panel panel-default">
-				<div class="panel-heading"><span class="text-uppercase">Usuarios</span></div>
+				<div class="panel-heading"><span class="text-uppercase">Categorias</span></div>
 				@include('partials.successMessage')
 				@include('categories.partials.filterForm')
 				<div class="panel-body">
 					<p>
-						<a class="btn btn-default" href="{{ route('categorias.create') }}" role="button"><i class="glyphicon glyphicon-plus"></i>&nbsp;&nbsp;&nbsp;Agregar Usuario</a>
+						<a class="btn btn-default" href="{{ route('categorias.create') }}" role="button"><i class="glyphicon glyphicon-plus"></i>&nbsp;&nbsp;&nbsp;Agregar Categoria</a>
 					</p>
 
 					@include('categories.partials.table')
 
 				</div>
 			</div>
-			{!! $users->render() !!}
-			<div class="alert alert-info"><span class="badge">{{ $users->total() }}</span> Usuarios Encontrados</div>
+			{!! $categories->render() !!}
+			<div class="alert alert-info"><span class="badge">{{ $categories->total() }}</span> Usuarios Encontrados</div>
 		</div>
 	</div>
 </div>
