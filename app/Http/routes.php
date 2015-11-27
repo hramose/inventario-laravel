18c6254ba472ea991b7bd5edaf192525b700137b
root <root@gosamt.sagelap>
2015-11-26 18:15:28 -0600

    se eñadden el resto de rutas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,8 +21,32 @@ Route::controllers([
 ]);
 
 //------------------------------------------------------------------------------
-//Usuarios
+//usuarios
 Route::group(['middleware' => ['auth', 'solo_admin'], 'namespace' => 'Users'], function () {
 	Route::resource('usuarios','UsersController');
+});
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+//categorias
+Route::group(['middleware' => 'auth', 'namespace' => 'Categories'], function () {
+	Route::resource('categorias','CategoriesController');
+});
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+//marcas
+Route::group(['middleware' => 'auth', 'namespace' => 'Brands'], function () {
+	Route::resource('marcas','BrandsController');
+});
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+//productos
+Route::group(['middleware' => 'auth', 'namespace' => 'Products'], function () {
+	Route::resource('productos','ProductsController');
+});
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+//productos
+Route::group(['middleware' => 'auth', 'namespace' => 'Loans'], function () {
+	Route::resource('prestamos','LoansController');
 });
 //------------------------------------------------------------------------------
