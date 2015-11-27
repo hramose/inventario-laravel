9878cd566b2958701dcfa7e13166b1985229a5f0
sage393 <sage393@gmail.com>
2015-11-27 10:10:59 -0600

    se camcbio texto

--- resources/views/categories/find.blade.php
+++ resources/views/categories/find.blade.php
@@ -1,18 +1,18 @@
 @extends('app')
 @section('title')
-Usuarios
+Categorías
 @endsection
 @section('content')
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-md-12">
 			<div class="panel panel-default">
-				<div class="panel-heading"><span class="text-uppercase">Usuarios (Busqueda)</span></div>
+				<div class="panel-heading"><span class="text-uppercase">Categorías (Busqueda)</span></div>
 				@include('partials.successMessage')
 				@include('categories.partials.filterForm')
 				<div class="panel-body">
 					<p>
-						<a class="btn btn-default" href="{{ route('categorias.create') }}" role="button"><i class="glyphicon glyphicon-plus"></i>&nbsp;&nbsp;&nbsp;Nuevo Usuario</a>
+						<a class="btn btn-default" href="{{ route('categorias.create') }}" role="button"><i class="glyphicon glyphicon-plus"></i>&nbsp;&nbsp;&nbsp;Nueva Categoría</a>
 					</p>
 
 					@include('categories.partials.table')
