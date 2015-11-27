4fa128197a3cf2b7ec64b4e4d569de9f8f86f288
root <root@gosamt.sagelap>
2015-11-26 18:07:06 -0600

    se crearon validaciones para editar usuario

---
+++ app/Http/Requests/EditUserRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Inventario\Http\Requests;
+
+use Inventario\Http\Requests\Request;
+
+class EditUserRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+          'name' 		 => 'required',
+          'email' 	 => 'required',
+          'carnet' 	 => 'required',
+          'password' => 'required|confirmed',
+          'type' 		 => 'required'
+        ];
+    }
+}
