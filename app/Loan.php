3f4758ceda8942d47920cbcd15a0b9bf72fb9f0d
sage393 <sage393@gmail.com>
2015-11-27 18:58:02 -0600

    se parsearon fechas

--- app/Loan.php
+++ app/Loan.php
@@ -3,21 +3,22 @@
 use Illuminate\Database\Eloquent\Model;
 
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Carbon\Carbon;
 
 class Loan extends Model {
 
 	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
+	* The database table used by the model.
+	*
+	* @var string
+	*/
 	protected $table = 'loans';
 
 	/**
-	 * The attributes that are mass assignable.
-	 *
-	 * @var array
-	 */
+	* The attributes that are mass assignable.
+	*
+	* @var array
+	*/
 	protected $fillable = ['cantidad', 'descripcion', 'inicio_prestamo', 'fin_prestamo', 'products_id'];
 
 	/**
@@ -32,5 +33,62 @@ class Loan extends Model {
 	public function products(){
 		return $this->belongsTo('Inventario\Product');
 	}
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+	/**
+	* inicio_prestamo
+	*/
+	public function setInicioPrestamoAttribute($value)
+	{
+		if (empty($value) || $value == 'Fecha no registrada')
+		{
+			$this->attributes['inicio_prestamo'] = Carbon::createFromFormat('d/m/Y', '00/00/0000')->toDateString();
+		}
+		else
+		{
+			$this->attributes['inicio_prestamo'] = Carbon::createFromFormat('d/m/Y', $value)->toDateString();
+		}
+	}
+//------------------------------------------------------------------------------
+	public function getInicioPrestamoAttribute($value)
+	{
+		if (Carbon::parse($value)->format('d/m/Y') == '30/11/-0001' || $value == null)
+		{
+			return 'Fecha no registrada';
+		}
+		else
+		{
+			return Carbon::parse($value)->format('d/m/Y');
+		}
+	}
+//-------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------
+	/**
+	* fin_prestamo
+	*/
+	public function setFinPrestamoAttribute($value)
+	{
+		if (empty($value) || $value == 'Fecha no registrada')
+		{
+			$this->attributes['fin_prestamo'] = Carbon::createFromFormat('d/m/Y', '00/00/0000')->toDateString();
+		}
+		else
+		{
+			$this->attributes['fin_prestamo'] = Carbon::createFromFormat('d/m/Y', $value)->toDateString();
+		}
+	}
+//------------------------------------------------------------------------------
+	public function getFinPrestamoAttribute($value)
+	{
+		if (Carbon::parse($value)->format('d/m/Y') == '30/11/-0001' || $value == null)
+		{
+			return 'Fecha no registrada';
+		}
+		else
+		{
+			return Carbon::parse($value)->format('d/m/Y');
+		}
+	}
+//------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
 }
