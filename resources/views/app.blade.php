073efde47debd8660d56eb5b8ac27650f94f5055
sage393 <sage393@gmail.com>
2015-11-27 18:44:13 -0600

    se incluye js propio

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -60,5 +60,6 @@
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+	<script src="{{ asset('js/main.js') }}"></script>
 </body>
 </html>
