218a4c76d8e72e94c01e0c0bd0b895299913ada7
sage393 <sage393@gmail.com>
2015-11-30 11:57:54 -0600

    se crea plantilla para devoluciones

---
+++ resources/views/loans/giveback.blade.php
@@ -0,0 +1,27 @@
+@extends('app')
+@section('title')
+Editar Prestamo
+@endsection
+
+@section('content')
+<div class="container">
+	<div class="row">
+		<div class="col-md-10 col-md-offset-1">
+			<div class="panel panel-default">
+				<div class="panel-heading">Editar Prestamo</div>
+
+				@include('partials.validateMessage')
+
+				<div class="panel-body">
+					{!! Form::model($loan, ['route' => ['prestamos.update', $loan], 'method' => 'PUT']) !!}
+
+					@include('loans.partials.fieldsFechaFin')
+
+					<button type="submit" class="btn btn-default">Actualizar</button>
+					{!! Form::close() !!}
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
