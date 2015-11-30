b4ed6f1342924ebf9c128612504e6937488914d6
sage393 <sage393@gmail.com>
2015-11-29 21:22:32 -0600

    se agregaron items al menu

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,8 +15,8 @@
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 	<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
 </head>
 <body>
@@ -39,15 +39,62 @@
 
 				<ul class="nav navbar-nav navbar-right">
 					@if (Auth::guest())
-						<li><a href="{{ url('/auth/login') }}">Login</a></li>
-						<!-- <li><a href="{{ url('/auth/register') }}">Register</a></li> -->
+					<li><a href="{{ url('/auth/login') }}">Login</a></li>
+					<!-- <li><a href="{{ url('/auth/register') }}">Register</a></li> -->
 					@else
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
-							<ul class="dropdown-menu" role="menu">
-								<li><a href="{{ url('/auth/logout') }}">Logout</a></li>
-							</ul>
-						</li>
+					{{-- Usuarios --}}
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Usuarios<span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<li><a href="{{ url('/usuarios/create') }}">Agregar Usuario</a></li>
+							<li><a href="{{ url('/usuarios') }}">Listar Usuarios</a></li>
+						</ul>
+					</li>
+					{{-- Fin Usuarios --}}
+					{{-- Marcas --}}
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Marcas<span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<li><a href="{{ url('/marcas/create') }}">Agregar Marca</a></li>
+							<li><a href="{{ url('/marcas') }}">Listar Marcas</a></li>
+						</ul>
+					</li>
+					{{-- Fin Marcas --}}
+					{{-- Categorías --}}
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Categorías<span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<li><a href="{{ url('/categorias/create') }}">Agregar Categoría</a></li>
+							<li><a href="{{ url('/categorias') }}">Listar Categorías</a></li>
+						</ul>
+					</li>
+					{{-- Fin Categorías --}}
+					{{-- Productos --}}
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Productos<span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<li><a href="{{ url('/productos/create') }}">Agregar Producto</a></li>
+							<li><a href="{{ url('/productos') }}">Listar Productos</a></li>
+						</ul>
+					</li>
+					{{-- Fin Productos --}}
+					{{-- Prestamos --}}
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Prestamos<span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<li><a href="{{ url('/prestamos/create') }}">Agregar Prestamo</a></li>
+							<li><a href="{{ url('/prestamos') }}">Listar Prestamos</a></li>
+						</ul>
+					</li>
+					{{-- Fin Prestamos --}}
+					{{-- Usuario --}}
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<li><a href="{{ url('/auth/logout') }}">Logout</a></li>
+						</ul>
+					</li>
+					{{-- Fin Usuario --}}
 					@endif
 				</ul>
 			</div>
