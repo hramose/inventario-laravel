da54bfb94dc507d3a13240cb395abb5317b114da
sage393 <sage393@gmail.com>
2015-11-30 11:53:09 -0600

    se agrego boton de fecha de devolución

--- resources/views/loans/partials/table.blade.php
+++ resources/views/loans/partials/table.blade.php
@@ -27,7 +27,14 @@
 				<td>{{ $loan->cantidad }}</td>
 				<td>{{ $loan->descripcion }}</td>
 				<td>{{ $loan->inicio_prestamo }}</td>
-				<td>{{ $loan->fin_prestamo }}</td>
+				<td>
+					@if (strcmp ($loan->fin_prestamo, 'Fecha no registrada' ) == 0 )
+						<a class="btn btn-default text-center btn-sm  btn-sp" href="{{ url('prestamos/'. $loan->id .'/give-back') }}" onclick="return confirm('¿Seguro que desea registrar devolución?')"><i class="glyphicon glyphicon-calendar"></i>&nbsp;&nbsp;&nbsp;Registrar devolución</a>
+					@else
+						{{ $loan->fin_prestamo }}
+					@endif
+				</td>
+
 				<td class="col-md-2">
 					<ul class="list-inline  text-center">
 						<li><a class="btn btn-default  btn-sm  btn-sp" href="{{ route('prestamos.edit', $loan) }}" onclick="return confirm('¿Seguro que desea editar?')"><i class="glyphicon glyphicon-pencil"></i>&nbsp;&nbsp;&nbsp;Editar</a></li>
