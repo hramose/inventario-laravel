b8a562bb6cdbd35a403346d3b360f1c2dd8eeea1
sage393 <sage393@gmail.com>
2015-11-30 20:52:56 -0600

    se agrega plantilla para página de inicio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,12 +7,11 @@ Inicio
 <div class="container">
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
-
-				<div class="panel-body">
-					You are logged in!
-				</div>
+			<div class="">
+				<h3>Bienvenido al sistema de inventario de la sala de redes</h3>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
 			</div>
 		</div>
 	</div>
