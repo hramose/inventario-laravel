8b11cde44d38593956893a26f41ffdf20a09f191
sage393 <sage393@gmail.com>
2015-12-01 22:08:35 -0600

    se agreggo mensaje de rror

--- resources/views/partials/successMessage.blade.php
+++ resources/views/partials/successMessage.blade.php
@@ -1,3 +1,10 @@
 @if (Session::has('message'))
 	<p class="alert alert-success">{{ Session::get('message') }}  <i class="glyphicon glyphicon-ok"></i> </p>
 @endif
+
+@if (Session::has('error'))
+<div class="alert alert-danger">
+	<p>{{ Session::get('error') }}  <i class="glyphicon glyphicon glyphicon-remove"></i> </p>
+	<p><a class="btn btn-default" href="{{ URL::previous() }}"><i class="glyphicon glyphicon-arrow-left"></i>&nbsp;&nbsp;&nbsp;&nbsp;Regresar</a></p>
+</div>
+@endif
