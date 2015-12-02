e404650b02d4e2319a0f89bfbb1f598617ac04ae
sage393 <sage393@gmail.com>
2015-12-01 22:41:27 -0600

    se elimino link para ir atras

--- resources/views/partials/successMessage.blade.php
+++ resources/views/partials/successMessage.blade.php
@@ -5,6 +5,5 @@
 @if (Session::has('error'))
 <div class="alert alert-danger">
 	<p>{{ Session::get('error') }}  <i class="glyphicon glyphicon glyphicon-remove"></i> </p>
-	<p><a class="btn btn-default" href="{{ URL::previous() }}"><i class="glyphicon glyphicon-arrow-left"></i>&nbsp;&nbsp;&nbsp;&nbsp;Regresar</a></p>
 </div>
 @endif
